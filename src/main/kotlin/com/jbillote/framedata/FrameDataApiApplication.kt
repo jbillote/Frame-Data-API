60b1dc3d8a033cf3b06537c178c00e29ed42744b
jbillote <jbillote@users.noreply.github.com>
2022-12-27 11:58:52 -0800

    Create IntelliJ project
    Add files generated by IntelliJ's Spring initializer

---
+++ src/main/kotlin/com/jbillote/framedata/FrameDataApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jbillote.framedata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrameDataApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<FrameDataApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jbillote/framedata/FrameDataApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jbillote.framedata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FrameDataApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
