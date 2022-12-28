2c34d5c0cb6df710f3c8d6c3de92da6d3ef16cf8
jbillote <jbillote@users.noreply.github.com>
2022-12-27 16:38:00 -0800

    Add character name map and move list JSON

---
+++ src/main/kotlin/com/jbillote/framedata/util/CharacterNameMap.kt
@@ -0,0 +1,28 @@
+package com.jbillote.framedata.util
+
+object CharacterNameMap {
+    val CHARACTER_NAME_MAP = mapOf(
+        "tohno" to "Shiki_Tohno",
+        "arcueid" to "Arcueid_Brunestud",
+        "akiha" to "Akiha_Tohno",
+        "ciel" to "Ciel",
+        "hisui" to "Hisui",
+        "kohaku" to "Kohaku",
+        "maids" to "Hisui_&_Kohaku",
+        "miyako" to "Miyako_Arima",
+        "kouma" to "Kouma_Kishima",
+        "noel" to "Noel",
+        "roa" to "Michael_Roa_Valdamjong",
+        "vlov" to "Vlov_Arkhangel",
+        "warc" to "Red_Arcueid",
+        "saber" to "Saber",
+        "aoko" to "Aoko_Aozaki",
+        "dan" to "Dead_Apostle_Noel",
+        "mario" to "Mario_Gallo_Bestino",
+        "pciel" to "Powered_Ciel",
+        "neco" to "Neco-Arc",
+        "mash" to "Mash_Kyrielight",
+        "ushi" to "Ushiwakamaru",
+        "dantes" to "Monte_Cristo"
+    )
+}
